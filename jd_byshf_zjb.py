1b7cdd9b5435255a2917c1ca52252bf01825202a
SheYu09 <46965259+SheYu09@users.noreply.github.com>
2021-10-07 17:02:49 +0800

    Update jd_byshf_zjb.py

--- jd_byshf_zjb.py
+++ jd_byshf_zjb.py
@@ -17,38 +17,43 @@ def getShareCode(purl, bodys1, bodys2, bodys3, header):
             encryptionInviterPin = r['data']['encryptionInviterPin']
             return encryptionInviterPin
     except Exception as e:
-        print("a获取互助码失败！", e)
-        print()
-        return 0
+        if aNum < 5:
+            aNum += 1
+            return getShareCode(purl, bodys1, bodys2, bodys3, header)
+        else:
+            aNum = 0
+            print("获取互助码失败！", e)
+            print()
+            return 0
 
 def helpCode(purl, bodys1, bodys2, bodys3, header, encryptionInviterPin, uNUm, user, name):
     print(f'====用户{uNUm} {user} 助力====')
+    print()
     try:
         body = f'{bodys1}participateInviteTask{bodys2},"encryptionInviterPin":"{encryptionInviterPin}","type":1{bodys3}'
         r = requests.post(url=purl, headers=header, data=body).json()
         if r['message'] == 'success':
-            print()
             print(r["message"])
             print(f'您也获得: {r["data"]["coinReward"]}金币')
         else:
-            print()
             print(r["message"])
-    except Exception as e:
         print()
+    except Exception as e:
         print("helpCode Error", e)
         print('报错了！')
+        print()
 
 def start():
-    global cookiesList, pinNameList, ckNum
     print('    ******* 百元生活费-赚金币-助力 *******')
+    print()
     cookiesList, pinNameList = jdCookie.start()
     for ckname in jdCookie.Name():
         try:
             ckNum = pinNameList.index(ckname)
         except:
             print(f"请检查被助力账号【{ckname}】名称是否正确？提示：助力名字可填pt_pin的值、也可以填账号名。")
+            print()
             continue
-        print()
         print(f"*******开始【京东账号】{pinNameList[int(ckNum)]} *******")
         print()
         purl, body1, body2 , body3 = posturl.jd_byshf()
@@ -64,10 +69,10 @@ def start():
                 u += 1
                 continue
             helpCode(purl, body1, body2, body3 , HEADERS.jd_byshf(i), encryptionInviterPin, u+1, pinNameList[u], pinNameList[ckNum])
-            print()
             time.sleep(0.1)
             u += 1
-  
+
+aNum = 0
 if __name__ == '__main__':
     start()
 

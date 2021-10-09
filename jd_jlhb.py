df74f58cb77e779079b676a1e97fa80140f1b4c5
SheYu09 <805608215@qq.com>
2021-10-09 20:52:07 +0800

    添加锦鲤

---
+++ jd_jlhb.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*
+'''
+项目名称: JD-Script / jd_jlhb
+活动名称: 锦鲤红包-助力
+Author: SheYu09
+cron: 0 0 * * * jd_jlhb.py
+new Env('京东 -*- 锦鲤红包')
+'''
+import requests, time, sys
+sys.path.append('../repo/SheYu09_jd_scripts_master/')
+import jdCookie, HEADERS, posturl
+
+def getShareCode(purl, bodys, header):
+    global aNum
+    try:
+        url = f'{purl}h5activityIndex{bodys}'
+        r = requests.post(url=purl, headers=header).json()
+        redPacketId = r["data"]["result"]["redpacketInfo"]["id"]
+        print(redPacketId)
+        return redPacketId
+    except Exception as e:
+        if aNum < 5:
+            aNum += 1
+            return getShareCode(purl, bodys, header)
+        else:
+            aNum = 0
+            print("获取互助码失败！", e)
+            print()
+            return 0
+
+def helpCode(purl, bodys, header, redPacketId, uNUm, user):
+    print(f'====用户{uNUm} {user} 助力====')
+    print()
+    try:
+        url = f'{purl}jinli_h5assist{bodys}'
+        body = 'body={' + f'"redPacketId":"{redPacketId}"' + '}'
+        r = requests.post(url=url, headers=header, data=body).json()
+        print(r)
+        print()
+    except Exception as e:
+        print("helpCode Error", e)
+        print('报错了！')
+        print()
+
+def start():
+    print('          ******* 锦鲤红包-助力 *******')
+    print()
+    cookiesList, pinNameList = jdCookie.start()
+    for ckname in jdCookie.Name():
+        try:
+            ckNum = pinNameList.index(ckname)
+        except:
+            print(f"请检查被助力账号【{ckname}】名称是否正确？提示：助力名字可填pt_pin的值、也可以填账号名。")
+            print()
+            continue
+        print(f"*******开始【京东账号】{pinNameList[int(ckNum)]} *******")
+        print()
+        purl, body = posturl.jd_jlhb()
+        redPacketId = getShareCode(purl, body, HEADERS.jd_jlhb(cookiesList[ckNum]))
+        if redPacketId == 0:
+            print(f"===【京东账号】{pinNameList[int(ckNum)]}  获取互助码失败。请稍后再试！==")
+            print()
+            time.sleep(0.1)
+            continue
+        u = 0
+        for i in cookiesList:
+            code = helpCode(purl, body, HEADERS.jd_jlhb(i), redPacketId, u+1, pinNameList[u])
+            time.sleep(0.1)
+            u += 1
+  
+aNum = 0
+if __name__ == '__main__':
+    start()
+
